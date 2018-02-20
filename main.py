37f2d8864f71a1a035e50962e8ac9636931275c9
katerinabc <kbohlec@gmail.com>
2018-02-20 15:37:41 +0000

    updated html files

--- main.py
+++ main.py
@@ -2,14 +2,20 @@
 # A very simple Flask Hello World app for you to get started with...
 
 from flask import Flask, request, render_template, redirect, send_from_directory
+from flask.ext.navigation import Navigation
 
 app = Flask(__name__)
+nav = Navigation(app)
 
 #@app.route('/')
 #def hello_world():
 #    return 'Hello from Flask!'
 
 ############LANDING PAGE & INSTRUCTIONS##################
+nav.Bar('top', [
+    nav.Item('Home', 'Dashboard', 'Data Collection', 'Human Capital Strategy'),
+])
+
 @app.route("/")
 def index():
     return render_template("index.html")
@@ -21,6 +27,10 @@ def socialmediadashboard():
 @app.route('/skillsdashboard')
 def skillsdashboard():
      return render_template('skillsdashboard.html')
+
+@app.route('/datasource')
+def datasorce():
+     return render_template('datasource.html')
 
 @app.route('/vacancydata')
 def vacancydata():
